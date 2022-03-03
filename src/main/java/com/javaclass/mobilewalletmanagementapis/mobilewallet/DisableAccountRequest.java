46ab0f9ddae5ac274c704d886e8e2cc7579c2d74
Cropcyc <48459764+Cropcyc@users.noreply.github.com>
2022-03-03 14:38:18 +0100

    add req&res for Disable&Update

--- src/main/java/com/javaclass/mobilewalletmanagementapis/mobilewallet/DisableAccountRequest.java
+++ src/main/java/com/javaclass/mobilewalletmanagementapis/mobilewallet/DisableAccountRequest.java
@@ -2,4 +2,52 @@ package com.javaclass.mobilewalletmanagementapis.mobilewallet;
 
 public class DisableAccountRequest{
     
+    private long requestId;
+    private String phoneNumber;
+    private String bankVerificationNumber;
+    private String emailAddress;
+
+    public DisableAccountRequest() {
+
+    }
+
+    public DisableAccountRequest(
+        long requestId,
+        String phoneNumber,
+        String bankVerificationNumber,
+        String emailAddress) {
+            this.requestId = requestId;
+            this.phoneNumber = phoneNumber;
+            this.bankVerificationNumber = bankVerificationNumber;
+            this.emailAddress = emailAddress;
+    }
+    
+    public long getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(long requestId) {
+        this.requestId = requestId;
+    }
+public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+public String getBankVerificationNumber() {
+        return bankVerificationNumber;
+    }
+
+    public void setBankVerificationNumber(String bankVerificationNumber) {
+        this.bankVerificationNumber = bankVerificationNumber;
+    }
+public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
 }
